35ae64e7ef8a77bbaa22580ed61ad503f015b0d0
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2022-12-05 05:56:28 -0500

    Update ReadyPlayerOne.java

--- ReadyPlayerOne.java
+++ ReadyPlayerOne.java
@@ -1,37 +1,38 @@
-import java.util.Random.*;
-
 public class GameRunner {
-	
 	public static String WinningSpy;
-	
 	public static void main(String[] args) {
 		Fight fight = new Fight();
+		fight.doBattle();
 		System.out.println("Winner is " + WinningSpy);
 	}
 }
 
-class  Fight {
-	static boolean BattleIsNotOver = true;
+class Fight {
+	boolean BattleIsOver = false;
 	Hero Johnny = new Hero();
 	Villian BorisAndNatasha = new Villian();
 
-	public static void doBattle() {
-		while (BattleIsNotOver) {
-			if ( Johnny.Health - Johnny.attack() < 0) {
-				GameRunner.WinningSpy = "Boris and Natasha";
-				BattleIsNotOver = false;
-			}
-			BorisAndNatasha.attack();
-			if ( BorisAndNatasha.Health - BorisAndNatasha.attack() < 0) {			
+	public void doBattle() {
+		while (! BattleIsOver) {
+			BorisAndNatasha.Health = BorisAndNatasha.Health - Johnny.attack();
+			if (BorisAndNatasha.Health < 0) {
 				GameRunner.WinningSpy = "Johnny Bravo";
-				BattleIsNotOver = false;			
+				BattleIsOver = true;
+			}
+			
+			Johnny.Health = Johnny.Health - BorisAndNatasha.attack();
+			if (Johnny.Health < 0) {
+				GameRunner.WinningSpy = "Boris and Natasha";
+				BattleIsOver = true;
+			}
+			
 		}
 	}
 }
 
 class Hero {
-	public static int Health = 100;
-	
+	public int Health = 100;
+
 	static int attack() {
 		java.util.Random rand = new java.util.Random();
 		int AttackStrength = rand.nextInt(100);
@@ -40,10 +41,9 @@ class Hero {
 }
 
 class Villian {
-	public static String villianName;
-	public static int Health;
+	public int Health = 100;
 
-	static int attack() {
+	int attack() {
 		java.util.Random rand = new java.util.Random();
 		int AttackStrength = rand.nextInt(100);
 		return AttackStrength;
